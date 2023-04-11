ac50a855890f1603ed1479f2156f23583646cc65
Zachary Beach <zacharybeach@Zacharys-MacBook-Pro-92.local>
2023-04-11 11:18:35 -0600

    all alembic issues fixed

--- app/db/session.py
+++ app/db/session.py
@@ -1,8 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+print(SQLALCHEMY_DATABASE_URL)
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+print(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- app/models/OffensivePlay.py
+++ app/models/OffensivePlay.py
@@ -1,10 +1,13 @@
+import uuid
+
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy.dialects.postgresql import UUID
 
 from app.db.base_class import Base
 
 
 class OffensivePlay(Base):
-    __tablename__ = "offensive_play"
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
     playType = Column(String, nullable=False)
     playId = Column(Integer, primary_key=True)
